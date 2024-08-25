9336094cdea77012fec5de62198d800f2406f208
Barath Balaji <barathb2306@gmail.com>
2024-08-25 18:08:04 -0400

    Add files via upload

---
+++ page.js
@@ -0,0 +1,44 @@
+import {AppBar, Container, Toolbar, Typography, Button, Box} from '@mui/material'
+import Link from 'next/link'
+import {SignIn} from '@clerk/nextjs'
+import SignUp from 'path-to-signup-component';
+
+export default function SignUpPage() {
+    return <Container maxWidth = "100vw">
+        <AppBar position = "static" sx = {{backgroundColor:  '#3f51b5'}} >
+            <Toolbar>
+                <Typography
+                    variant = 'h6'
+                    sx={{
+                        flexGrow: 1
+                    }}
+                    >
+                    Flashcard Saas
+                </Typography>
+                <Button color = "inherit">
+                <Link href = "sign-in" passHref>
+                    Login
+                </Link>
+                </Button>
+
+                <Button color = "inherit">
+                <Link href = "sign-up" passHref>
+                    Sign Up
+                </Link>
+                </Button>
+
+            </Toolbar>
+        </AppBar>
+
+        <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            >
+                <Typography variant = "h4">Sign Up</Typography>
+                <SignUp />
+            </Box>
+    </Container>
+                
+}
